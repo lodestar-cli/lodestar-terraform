edfde27bd1c9727d986c2e3ed3ce0711f50a4a6c
Simon Rall <rallinator7@gmail.com>
2021-04-23 10:40:30 -0500

    added basemint user

--- github/members/members.tf
+++ github/members/members.tf
@@ -4,7 +4,7 @@ resource "github_membership" "lodestar_bot" {
   role     = "member"
 }
 
-resource "github_team_membership" "lb_team_membership" {
+resource "github_team_membership" "lb_tester_membership" {
   team_id  = data.github_team.testers.id
   username = github_membership.lodestar_bot.username
   role     = "member"
@@ -16,8 +16,20 @@ resource "github_membership" "rallinator7" {
   role     = "admin"
 }
 
-resource "github_team_membership" "some_team_membership" {
+resource "github_team_membership" "rallinator7_admin_membership" {
   team_id  = data.github_team.admins.id
   username = github_membership.rallinator7.username
+  role     = "maintainer"
+}
+
+# basemint membership and teams
+resource "github_membership" "basemint" {
+  username = "basemint"
+  role     = "admin"
+}
+
+resource "github_team_membership" "basemint_admin_membership" {
+  team_id  = data.github_team.admins.id
+  username = github_membership.basemint.username
   role     = "maintainer"
 }
