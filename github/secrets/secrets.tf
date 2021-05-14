eb1a480bf021b8bca2be48477e229ed502badf6f
Simon Rall <rallinator7@gmail.com>
2021-05-14 11:31:44 -0500

    Added new secrets for github actions

--- github/secrets/secrets.tf
+++ github/secrets/secrets.tf
@@ -17,3 +17,27 @@ resource "github_actions_secret" "aws_access_key_secret" {
   secret_name      = "AWS_ACCESS_KEY_SECRET"
   plaintext_value  = data.sops_file.github_actions_sops_secrets.data["aws-access-key-secret"]
 }
+
+resource "github_actions_secret" "docker_username" {
+  repository       = "lodestar"
+  secret_name      = "DOCKER_HUB_USERNAME"
+  plaintext_value  = data.sops_file.github_actions_sops_secrets.data["docker-hub-username"]
+}
+
+resource "github_actions_secret" "docker_access_token" {
+  repository       = "lodestar"
+  secret_name      = "DOCKER_HUB_ACCESS_TOKEN"
+  plaintext_value  = data.sops_file.github_actions_sops_secrets.data["docker-hub-access-token"]
+}
+
+resource "github_actions_secret" "github_username" {
+  repository       = "lodestar"
+  secret_name      = "GIT_USER"
+  plaintext_value  = data.sops_file.github_actions_sops_secrets.data["github-username"]
+}
+
+resource "github_actions_secret" "github_access_token" {
+  repository       = "lodestar"
+  secret_name      = "GIT_TOKEN"
+  plaintext_value  = data.sops_file.github_actions_sops_secrets.data["github-token"]
+}
